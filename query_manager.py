bec2d5585b68ba4274a12799191232e483cc1901
Laurent Stanevich <laurent@stanevich.com>
2019-08-13 09:55:01 -0400

    Initial commit

---
+++ query_manager.py
@@ -0,0 +1,3 @@
+class QueryManager:
+    def __init__(self):
+        print("Initializing Query Manager")
---
+++ tests/test_query_manager.py
@@ -0,0 +1,6 @@
+from query_manager import QueryManager
+
+
+def test_query_manager():
+    qm = QueryManager()
+    assert True
