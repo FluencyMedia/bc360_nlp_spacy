ab18d4523ed3a007f6f9ac81077f0ab7e372e5d6
Laurent Stanevich <laurent@stanevich.com>
2019-08-13 14:14:36 -0400

    Moved to simpler set of file-handling generators

--- query_manager.py
+++ query_manager.py
@@ -2,51 +2,28 @@ from pathlib import Path
 import logging
 import glob
 import csv
+import os
 
 
-class QueryManager:
-    def __init__(self):
-        self._logger = logging.getLogger("QueryManager")
-        self._logger.info("Initializing Query Manager")
-        self._files = self.load_file_list()
+# Following generators cribbed from: https://realpython.com/introduction-to-python-generators/
+def generate_filenames():
+    """
+    generates a sequence of opened files
+    matching a specific extension
+    """
+    for dir_path, dir_names, file_names in os.walk("sources/"):
+        for file_name in file_names:
+            if file_name.endswith(".csv"):
+                yield open(os.path.join(dir_path, file_name))
 
-    def load_file_list(self):
-        # Following code cribbed from: https://stackoverflow.com/questions/53032118/reading-multiple-files-containing-multiple-csvs
 
-        p = Path.cwd() / "sources"
-
-        files = []
-        file_extensions = [".csv"]
-        for file_or_directory in p.iterdir():
-            if (
-                file_or_directory.is_file()
-                and "".join(file_or_directory.suffixes).lower() in file_extensions
-            ):
-                files.append(file_or_directory)
-            elif file_or_directory.is_dir():
-                files.extend(
-                    [
-                        x
-                        for x in file_or_directory.iterdir()
-                        if "".join(x.suffixes).lower() in file_extensions
-                    ]
-                )
-        self._logger.debug("About to print files")
-        self._logger.debug(f"{files}")
-        self._logger.debug("Finished printing files")
-        pass
-        return files
-
-    @property
-    def files(self):
-        return self._files
-
-    def scan_files(self):
-        for fl in self.files:
-            with fl.open(mode="r") as f:
-                reader = csv.reader(f)
-                for row in reader:
-                    logger.debug(row)
+def cat_files(files):
+    """
+    takes in an iterable of filenames
+    """
+    for fname in files:
+        for line in fname:
+            yield line
 
 
 if __name__ == "__main__":
@@ -56,16 +33,6 @@ if __name__ == "__main__":
         datefmt="%I:%M:%S %p",
     )
     logger = logging.getLogger("QueryManager")
-
-    lh_console = logging.StreamHandler()
-    lh_console.setLevel(logging.DEBUG)
-    # lh_console.setFormatter(
-    #     logging.Formatter(
-    #         fmt="%(asctime)s | %(name)s | %(levelname)s || %(message)s",
-    #         datefmt="%H:%M:%S",
-    #     )
-    # )
-    # logger.addHandler(lh_console)
 
     lh_file = logging.FileHandler(Path.cwd() / "logs" / "query_manager.log")
     lh_file.setLevel(logging.INFO)
@@ -77,9 +44,10 @@ if __name__ == "__main__":
     )
     logger.addHandler(lh_file)
 
-    logger.info("Starting 'query_manager'")
-    qm = QueryManager()
-    logger.info("'query_manager' loaded\n")
-    qm.scan_files()
+    csv_files = generate_filenames()
+    csv_file = cat_files(csv_files)
+    # lines = grep_files(csv_file, 'a')
+    for line in csv_file:
+        logger.debug(line)
 
     pass
