ac87c64cc680e411bbdcffcd3d8caa5ceb5b537c
Laurent Stanevich <laurent@stanevich.com>
2019-08-13 20:02:31 -0400

    Working on entities

--- query_manager.py
+++ query_manager.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import logging
 import csv
 import os
+import spacy
+from spacy import displacy
+
 
 # Following generators cribbed from: https://realpython.com/introduction-to-python-generators/
 def generate_filenames():
@@ -21,7 +24,41 @@ def cat_files(files):
     """
     for fname in files:
         for line in fname:
-            yield line
+            yield line.rstrip()
+
+
+def proc_queries(queries):
+    """
+    Serially processes queries
+    """
+
+    logger.debug("Clearing '\\renders\\'")
+    for dir_path, dir_names, file_names in os.walk("renders/"):
+        for file_name in file_names:
+            os.remove(os.path.join(dir_path, file_name))
+            logger.debug(f"Removed file: {file_name}")
+
+    nlp = spacy.load("en_core_web_sm")
+    i = 1
+
+    for current_query in queries:
+        print()
+        logger.info(f"QUERY: {current_query} [{len(current_query)}]")
+        q = nlp(current_query)
+        svg = displacy.render(q, style="dep", page=False)
+        fname = f"{i:08d} - {'_'.join([w.text for w in q if not w.is_punct])}" ".html"
+        # f = format(i, "08d") + "-".join([w.text for w in q if not w.is_punct]) + ".html"
+        output_path = Path.cwd().joinpath("renders") / fname
+        with output_path.open("w", encoding="utf-8") as f:
+            f.write(svg)
+        # displacy.serve(q, style="ent")
+        logger.info(f"Text | Lemma | PoS | Label | Tag | Dep_ | Shape | Alpha? | Stop?")
+        for token in q:
+            logger.info(
+                f"{token.text} | {token.lemma_} | {token.pos_} | {token} | {token.tag_} | {token.dep_} | {token.shape_} | {token.is_alpha} | {token.is_stop}"
+            )
+        i += 1
+        breakpoint()
 
 
 if __name__ == "__main__":
@@ -42,10 +79,9 @@ if __name__ == "__main__":
     )
     logger.addHandler(lh_file)
 
-    csv_files = generate_filenames()
-    csv_file = cat_files(csv_files)
+    query_files = generate_filenames()
+    q_file = cat_files(query_files)
     # lines = grep_files(csv_file, 'a')
-    for line in csv_file:
-        logger.debug(line)
+    proc_queries(q_file)
 
     pass
